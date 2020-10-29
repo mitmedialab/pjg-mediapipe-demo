bcb81d92632404bb153163946e769b2b0a7c9245
DWAYNE JONES <Dwaynekj@gmail.com>
2020-10-29 17:33:58 -0400

    Tested Mediapipe headless via local docker

--- README.md
+++ README.md
@@ -1,13 +1,21 @@
 # pjg-mediapipe-demo
 Demo of media pipe python integration with Django
 
-# Server Setup
+## Server Setup
+
+### GCP 
 - Temporary Server (Dwayne's Personal) `gcloud beta compute ssh --zone "us-east4-c" "mediapipe-server" --project "mystical-slate-241320"`
 - Follow https://google.github.io/mediapipe/getting_started/install.html#installing-on-debian-and-ubuntu
+  - and https://dev.classmethod.jp/articles/mediapipe-install-on-aws-ec2-with-gpu-english/
 - `GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hello_world:hello_world`
 
-# Docker
+### Docker / Local
 - Taken from https://google.github.io/mediapipe/getting_started/install.html
 - `docker build --tag=mediapipe .`
 - `docker run -it --name mediapipe mediapipe:latest`
 - `root@bca08b91ff63:/mediapipe# GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hello_world:hello_world`
+
+## Test Data
+- https://a-counting-sign-staging.herokuapp.com/transcribe/test/test_fist
+- https://a-counting-sign-staging.herokuapp.com/transcribe/test/test_other_heuristics
+- https://a-counting-sign-language.s3.amazonaws.com/demos/main_demos_mp4/fist_only/12.mp4
--- serve.py
+++ serve.py
@@ -2,10 +2,12 @@ import subprocess
 from flask import Flask, request
 app = Flask(__name__)
 
-#https://google.github.io/mediapipe/solutions/hands.html#desktop
-cpu_bash_command = "GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hand_tracking:hand_tracking_cpu"
-gpu_bash_command = "GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hand_tracking:hand_tracking_gpu"
-
+# https://google.github.io/mediapipe/solutions/hands.html#desktop
+hello_world_build_command = "GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hello_world:hello_world"
+cpu_build_bash_command = "GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hand_tracking:hand_tracking_cpu"
+gpu_build_bash_command = "GLOG_logtostderr=1 bazel run --define MEDIAPIPE_DISABLE_GPU=1 mediapipe/examples/desktop/hand_tracking:hand_tracking_gpu"
+# https://google.github.io/mediapipe/getting_started/building_examples.html#option-1-running-on-cpu
+cpu_run_command = "GLOG_logtostderr=1 bazel-bin/mediapipe/examples/desktop/hand_tracking/hand_tracking_cpu --calculator_graph_config_file=mediapipe/graphs/hand_tracking/hand_tracking_desktop_live.pbtxt"
 
 @app.route('/')
 def hello():
@@ -17,11 +19,13 @@ def hand():
     if video.filename != '':
         video.save(video.filename)
 
-    cmd = cpu_bash_command + f'--input_video_path="{video.filename}" --output_video_path="out-{video.filename}"'
+    cmd = cpu_run_command + f'--input_video_path="{video.filename}" --output_video_path="out-{video.filename}"'
     process = subprocess.Popen(cmd.split(), stdout=subprocess.PIPE)
     output, error = process.communicate()
     print(output)
     print(f'error: {error}')
+    # Write file to s3
+    # Delete both files or stream them
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
